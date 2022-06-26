7ccda8383c8e62f9822f07b5eca299e97a484f61
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-26 06:33:14 +0600

    show student by search

--- app/Http/Controllers/Backend/Student/StudentRollController.php
+++ app/Http/Controllers/Backend/Student/StudentRollController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\AssignStudent;
 use App\Models\StudentClass;
 use App\Models\StudentYear;
 use Illuminate\Http\Request;
@@ -15,5 +16,15 @@ class StudentRollController extends Controller
         $years = StudentYear::all();
     
         return view('backend.student.roll.view_roll_generator', compact('classes', 'years'));
+    }
+
+    //View student roll generate
+    public function getStudents(Request $request){
+        $classes = StudentClass::all();
+        $years = StudentYear::all();
+    
+        $students = AssignStudent::with(['student'])->where('year_id',$request->year_id)->where('class_id',$request->class_id)->get();
+  
+      return response()->json($students);
     }
 }
